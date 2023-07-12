de08529e4122a836c25769d7d2e1530844cca259
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-07-12 20:41:19 +0545

    get installed apps regardless of android version

--- app/src/main/java/com/example/appquota/MainActivity.kt
+++ app/src/main/java/com/example/appquota/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.appquota
 
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import android.os.Build
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appquota.ui.theme.AppQuotaTheme
 
+//class MainActivity : AppCompatActivity() {
+// if you get installed apps in MainActivity, it needs to extend AppCompatActivity()
+// val context: Context = this  // might need to initialize context like this. idk
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,11 +35,26 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
 fun AppQuota(modifier: Modifier = Modifier) {
 
+    // gets all apps including system apps. baal ho for now
+    // this should prob go in MainActivity
+    val context = LocalContext.current
+    val pm = context.packageManager
+    val packages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        pm.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
+    } else {
+        pm.getInstalledApplications(0)
+    }
+
+    for (packageInfo in packages) {
+        Log.d(TAG, "Package name:" + packageInfo.packageName)
+    }
+    Text("Test")
 }
 
 @Preview(showBackground = true)
