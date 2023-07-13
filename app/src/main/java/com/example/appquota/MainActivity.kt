ae9d771a238ef817312b0ca9f61467320399a4fd
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-07-13 07:30:04 +0545

    get installed apps outside of Composable
    MainActivity can inherit AppCompatActivity instead of ComponentActivity without any issues

--- app/src/main/java/com/example/appquota/MainActivity.kt
+++ app/src/main/java/com/example/appquota/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.appquota
 
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appquota.ui.theme.AppQuotaTheme
 
-//class MainActivity : AppCompatActivity() {
-// if you get installed apps in MainActivity, it needs to extend AppCompatActivity()
-// val context: Context = this  // might need to initialize context like this. idk
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,24 +34,26 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    fun getApps(): MutableList<ApplicationInfo> {
+        // gets all apps including system apps. baal ho for now
+        val context: Context = this
+        val pm = context.packageManager
+        // due to API changes in android 13 (TIRAMISU/ API 33),
+        val packages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
+        } else {
+            pm.getInstalledApplications(0)
+        }
+
+        for (packageInfo in packages) {
+            Log.d(TAG, "Package name:" + packageInfo.packageName)
+        }
+        return packages
+    }
 }
 
 @Composable
 fun AppQuota(modifier: Modifier = Modifier) {
-
-    // gets all apps including system apps. baal ho for now
-    // this should prob go in MainActivity
-    val context = LocalContext.current
-    val pm = context.packageManager
-    val packages = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        pm.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L))
-    } else {
-        pm.getInstalledApplications(0)
-    }
-
-    for (packageInfo in packages) {
-        Log.d(TAG, "Package name:" + packageInfo.packageName)
-    }
     Text("Test")
 }
 
