377f659fca14ff47b307333cb04982bd2dea4243
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-07-19 08:14:03 +0545

    display countdown in block screen

--- app/src/main/java/com/example/appquota/AppBlockedActivity.kt
+++ app/src/main/java/com/example/appquota/AppBlockedActivity.kt
@@ -1,6 +1,8 @@
 package com.example.appquota
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,26 +10,71 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.appquota.ui.theme.AppQuotaTheme
 
 class AppBlockedActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        val remainingSeconds = intent.getLongExtra("remaining_seconds", 0)
+//        TODO this value isn't being sent properly & keeps using default 0
+
         setContent {
-            AppQuotaTheme() {
+            AppQuotaTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppBlockedScreen()
+//                    TODO maybe conditionally call this above composable if block_time_finished boolean is false?
                 }
             }
         }
+
+//        if (intent.getBooleanExtra("block_time_finished", false)) {
+//            Toast.makeText(this, "Blocked time is finished", Toast.LENGTH_SHORT).show()
+////            hmmm this toast triggered properly. need to implement the starting time limit so that block screen isn't shown constantly
+//            finish()
+//        }
     }
 }
 
 @Composable
 fun AppBlockedScreen(modifier: Modifier = Modifier) {
-    Text("Stop right there, criminal scum!")
+    var countdownTime by remember { mutableStateOf(10) }
+
+    DisposableEffect(Unit) {
+        val timer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                countdownTime = (millisUntilFinished / 1000).toInt()
+            }
+
+            override fun onFinish() {
+                countdownTime = 0
+//                TODO now I need to finish this activity. either declare countdownTimer in Activity class body & finish there
+//                or find a way to finish here
+            }
+        }
+        timer.start()
+
+        onDispose {
+            timer.cancel()
+        }
+    }
+//    val remainingTimeState = rememberUpdatedState(remainingSeconds)
+    Text("You can't access this app for $countdownTime more seconds")
+}
+
+@Preview
+@Composable
+fun AppBlockedScreenPreview() {
+//    AppBlockedScreen(modifier = Modifier.fillMaxSize())
 }
--- app/src/main/java/com/example/appquota/AppLaunchAccessibilityService.kt
+++ app/src/main/java/com/example/appquota/AppLaunchAccessibilityService.kt
@@ -1,8 +1,10 @@
 package com.example.appquota
 
 import android.accessibilityservice.AccessibilityService
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.CountDownTimer
 import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
@@ -13,6 +15,13 @@ class AppLaunchAccessibilityService : AccessibilityService() {
     private lateinit var serviceContext: Context
     private var lastPackageName: String? = null
     private var blockedApp: String? = null
+    private var firstLaunch = true
+    private var remainingBlockTime: Long = 0
+    // TODO rename this to blockTime cause needs to be constant value instead of changing?
+    private var countDownTimer: CountDownTimer? = null
+    private var appBlockedActivity: Activity? = null
+    private var blockTimeFinished = false
+    private var intent: Intent? = null
 
     override fun onServiceConnected() {
         serviceContext = this
@@ -29,20 +38,24 @@ class AppLaunchAccessibilityService : AccessibilityService() {
                 // done because TYPE_WINDOW_STATE_CHANGED also triggers when activity changed in the
                 // same app & we only need to detect app switching
                 lastPackageName = packageName
-//                Toast.makeText(this, "app switch detected", Toast.LENGTH_SHORT).show()
-                runBlocking {
-                    blockedApp = serviceContext.dataStore.data.first()[BLOCKED_APP_KEY]
-                }
+                runBlocking { blockedApp = serviceContext.dataStore.data.first()[BLOCKED_APP_KEY] }
+
                 if (blockedApp.equals(packageName)) {
                     Log.d("DataStoreExample", "If you see this, then fuck yeah")
                     Toast.makeText(this, "switch to blocked app detected", Toast.LENGTH_SHORT).show()
+                    intent = Intent(this, AppBlockedActivity::class.java)
+                    if (firstLaunch) {
+                        firstLaunch = false
+//                        remainingBlockTime = 1200000  // 20 minutes in milliseconds
+                        remainingBlockTime = 15000  // 5seconds
+//                        startTimer()
+//                    } else {
+//                        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                    }
+                    intent?.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP
 
-                    val intent = Intent(this, AppBlockedActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-                    // Add any extra data to the intent if needed
-                    // intent.putExtra("key", value)
-
+//                    TODO move intent initialization to onCreate instead? because this runs on every switch to blocked app
+//                    intent?.putExtra("block_time_finished", blockTimeFinished)
                     startActivity(intent)
                 }
             }
@@ -51,5 +64,35 @@ class AppLaunchAccessibilityService : AccessibilityService() {
 
     override fun onInterrupt() {
         // This method is called when the accessibility service is interrupted or disabled.
+    }
+
+    private fun startTimer() {
+        // Cancel any existing timer before starting a new one
+        cancelTimer()
+
+        countDownTimer = object : CountDownTimer(remainingBlockTime, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                intent?.putExtra("remaining_seconds", millisUntilFinished / 1000)
+                // You could update UI with the remaining time if needed
+            }
+
+            override fun onFinish() {
+                // Time's up, hide the activity with the text
+                hideRestrictedActivity()
+            // TODO in the end, if hideRestrictedActivity doesn't have much code, move it here & delete function
+            }
+        }.start()
+    }
+
+    private fun cancelTimer() {
+        countDownTimer?.cancel()
+        countDownTimer = null
+    }
+
+    private fun hideRestrictedActivity() {
+        blockTimeFinished = true
+        // Implement the logic to hide the activity with the text here
+        // For example, you can use the WindowManager to remove the view or finish the activity
+        // The exact implementation depends on how you are displaying the restricted activity.
     }
 }
